fc800d5aff8ff43a992f835757a8f071fa2c1d0e
jramonsanchez <joseramonsanchezsolorzano@gmail.com>
2019-11-16 11:17:27 -0600

    Create ConocimientoEnfermedades.pl

---
+++ ConocimientoEnfermedades.pl
@@ -0,0 +1,8 @@
+
+enfermedad('Resfriado',['Escurrimiento Nasal','Ojos Llorosos','Estornudos']).
+enfermedad('Influenza',['Fiebre','Dolor del Cuerpo','Dolor de Cabeza']).
+enfermedad('Bronquitis',['Tos','Dolor del Pecho','Moco Espeso']).
+enfermedad('Faringitis',['Fiebre','Dolor del Garganta','Imflamacion de Amigdalas']).
+enfermedad('Neumonia',['Fiebre','Dificultad para Respirar','Tos']).
+enfermedad('Asma',['Tos','Sibilancias','Presion en el Pecho','Dificultad para Respirar']).
+
